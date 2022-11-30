ded1167eb66a902a0df335c826613140141100af
canerkocak <canerotellie@gmail.com>
2022-11-30 23:09:08 +0100

    aaaaaaa

--- app.py
+++ app.py
@@ -1,40 +1,53 @@
-from flask import Flask
-from flask import render_template
-# from osrs_api import Hiscores
+from flask import Flask, render_template, request, redirect, url_for
+import pickle
 
+    
 app = Flask(__name__)
+try:
+    with open('players.pickle', 'rb') as f:
+        players = pickle.load(f)
+except:
+    players = {}
+    with open('players.pickle', 'wb') as f:
+        pickle.dump(players, f)
 
-# def get_player_skills(player_name):
-#     player = Hiscores(player_name)
-#     total_level = 0
-#     skills = player.skills
-#     for skill in skills:
-#         skill = skills[skill]
-#         total_level += skill.level
-#     return total_level
 
-# def get_player_total_xp(player_name):
-#     player = Hiscores(player_name)
-#     total_xp = 0
-#     skills = player.skills
-#     for skill in skills:
-#         skill = skills[skill]
-#         total_xp += skill.xp
-#     return total_xp
-
-# class Player:
-#     def __init__(self, name):
-#         self.name = name
-
-#     @property
-#     def total_level(self):
-#         return get_player_skills(self.name)
+def add_kill(player, npc, timestamp, price, items):
+    if player not in players:
+        players[player] = {}
+    if npc not in players[player]:
+        players[player][npc] = []
+    players[player][npc].append({'timestamp': timestamp, 'price': price, 'items': items})
+    with open('players.pickle', 'wb') as f:
+        pickle.dump(players, f)
 
 @app.route("/")
-# def hello_world():
-#     players = [Player("Gimmy Woo"), Player("Stoffell"), Player("GroupQuobii"), Player("DummyNeutron")]   
-    return render_template('index.html')
+def hello_world():
+    return render_template('index.html', players=players)
+
+@app.route('/<player>/inventory_items/', methods=['POST'])
+def inventory_items(player):
+    return '', 200
+
+@app.route('/<player>/equipped_items/', methods=['POST'])
+def equipped_items(player):
+    return '', 200
+
+@app.route('/<player>/npc_kill/', methods=['POST'])
+def npc_kill(player):
+    data = request.json
+    timestamp = data['timestamp']
+    data = data['data']
+    price =  data['gePrice']
+    npcID = data['npcId']
+    items = data['items']
+    add_kill(player, npcID, timestamp, price, items)
+    return '', 200
 
 
+    
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+    
+    
+# make html that prints out the players and their kills in a table
